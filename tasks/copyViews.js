509ed5e6a0d8c0647edaab9e0317c91ef32730ff
netherwaves <avery@netherwaves.com>
2022-05-31 16:09:33 -0400

    make the pack script better

--- tasks/copyViews.js
+++ tasks/copyViews.js
@@ -1,9 +1,21 @@
+const fs = require("fs");
 const fse = require("fs-extra");
 const path = require("path");
 
+// directory paths
 const srcDir = path.join(__dirname, "../views");
 const destDir = path.join(__dirname, "../dist/public");
 
-fse.copySync(srcDir, destDir, { overwrite: true });
 
-console.log("done copying views...");
+// copy function
+function copyDir() {
+    console.log("copying views...");
+    // copy
+    fse.copySync(srcDir, destDir, { overwrite: true });
+    console.log("done copying views!");
+}
+
+
+// watch for changes
+fs.watch(srcDir, (event, filename) => copyDir());
+copyDir();
