898bba9a991b36fc39b9a36945f466ade356968b
netherwaves <avery@netherwaves.com>
2022-06-16 15:48:39 -0400

    improve asset management

--- tasks/copyViews.js
+++ tasks/copyViews.js
@@ -1,21 +1,26 @@
+// imports
 const fs = require("fs");
 const fse = require("fs-extra");
 const path = require("path");
 
 // directory paths
-const srcDir = path.join(__dirname, "../views");
+const viewsDir = path.join(__dirname, "../views");
+const assetsDir = path.join(__dirname, "../assets");
 const destDir = path.join(__dirname, "../dist/public");
 
 
-// copy function
-function copyDir() {
-    console.log("copying views...");
+// copy functions
+function copyDir(src, dest) {
+    console.log(`copying ${src}...`);
     // copy
-    fse.copySync(srcDir, destDir, { overwrite: true });
-    console.log("done copying views!");
+    fse.copySync(src, dest, { overwrite: true });
+    console.log("done copying!");
 }
 
-
 // watch for changes
-fs.watch(srcDir, (event, filename) => copyDir());
-copyDir();
+fs.watch(viewsDir, (event, filename) => copyDir(viewsDir, destDir));
+fs.watch(assetsDir, (event, filename) => copyDir(assetsDir, destDir));
+
+// execute once
+copyDir(viewsDir, destDir);
+copyDir(assetsDir, destDir);
